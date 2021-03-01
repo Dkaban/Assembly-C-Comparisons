478812994ee5b3b313a6ec792c36025dc90adb77
Dustin <dustinkaban@gmail.com>
2021-03-01 09:06:39 -0800

    Create sumWhileLoop.asm

---
+++ sumWhileLoop.asm
@@ -0,0 +1,47 @@
+        .file   "sumWhileLoop.c"
+        .text
+.globl sumWhileLoop
+        .type   sumWhileLoop, @function
+sumWhileLoop:
+.LFB12:
+        .cfi_startproc
+        movl    $0, %eax
+        cmpl    %esi, %edi
+        jg      .L3
+.L6:
+        addl    %edi, %eax
+        addl    $1, %edi
+        cmpl    %edi, %esi
+        jge     .L6
+.L3:
+        rep; ret
+        .cfi_endproc
+.LFE12:
+        .size   sumWhileLoop, .-sumWhileLoop
+        .section        .rodata.str1.1,"aMS",@progbits,1
+.LC0:
+        .string "Sum: %d"
+        .text
+.globl main
+        .type   main, @function
+main:
+.LFB11:
+        .cfi_startproc
+        subq    $8, %rsp
+        .cfi_def_cfa_offset 16
+        movl    $10, %esi
+        movl    $3, %edi
+        call    sumWhileLoop
+        movl    %eax, %esi
+        movl    $.LC0, %edi
+        movl    $0, %eax
+        call    printf
+        movl    $0, %eax
+        addq    $8, %rsp
+        .cfi_def_cfa_offset 8
+        ret
+        .cfi_endproc
+.LFE11:
+        .size   main, .-main
+        .ident  "GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-23)"
+        .section        .note.GNU-stack,"",@progbits
